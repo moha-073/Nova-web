84387e199335cbedb70deef929998103e6fc07fd
Mohamed Abdi <mohamedabdimoha2411@gmail.com>
2026-03-03 23:33:35 +0300

    Edit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath } from 'node:url'
+import { dirname } from 'node:path'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: import.meta.env.VITE_BASE_PATH || "/Nova-web"
-})
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+export default ({ mode }) => {
+  // Load env variables
+  const env = loadEnv(mode, __dirname, 'VITE_');
+
+  return defineConfig({
+    base: env.VITE_BASE_PATH || '/', // now VITE_BASE_PATH is read correctly
+    plugins: [react(), tailwindcss()],
+  });
+};
