4618db09bc01fd2523752c5205db648597c48a83
Mohamed Abdi <mohamedabdimoha2411@gmail.com>
2026-03-05 17:07:32 +0300

    Fix image modes

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -24,18 +24,18 @@ const Navbar = () => {
       <div className="flex items-center ml-5 space-x-2 font-mono text-sm md:text-lg text-slate-400 dark:text-slate-300 hover:text-sky-700">
         <img className="mt-2 h-7 w-7 md:h-9 md:w-9" src="https://cdn.dribbble.com/userupload/43346116/file/original-8f140c0ebaf9d7ffcb9f007b517b3a40.jpg?format=webp&resize=400x300&vertical=center"  alt="Logo" />
         <a href="#" className=" hover:text-gray-300" >  Nova-Web</a></div>
-      <div className="font-mono text-sm md:text-lg cursor-pointer dark:text-slate-300 hover:text-sky-700"><NavLink to="/">Home</NavLink></div>
-      <div className="font-mono text-sm md:text-lg cursor-pointer dark:text-slate-300 hover:text-sky-700"><NavLink to="/services">Services</NavLink></div>
-      <div className="font-mono text-sm md:text-lg cursor-pointer dark:text-slate-300 hover:text-sky-700"><NavLink to="/about">About</NavLink></div>
-      <div className="font-mono text-sm md:text-lg cursor-pointer dark:text-slate-300 hover:text-sky-700"><NavLink to="/contact">Contact</NavLink></div>
-      <div className="font-mono text-sm md:text-lg cursor-pointer dark:text-slate-300 hover:text-sky-700">FAQ</div>
-      <button className="px-2 py-1 md:px-4 md:py-2 text-green-900 bg-white rounded-md cursor-pointer hover:bg-green-800 hover:text-white">Get Started</button>
+      <div className="font-mono text-sm cursor-pointer md:text-lg dark:text-slate-300 hover:text-sky-700"><NavLink to="/">Home</NavLink></div>
+      <div className="font-mono text-sm cursor-pointer md:text-lg dark:text-slate-300 hover:text-sky-700"><NavLink to="/services">Services</NavLink></div>
+      <div className="font-mono text-sm cursor-pointer md:text-lg dark:text-slate-300 hover:text-sky-700"><NavLink to="/about">About</NavLink></div>
+      <div className="font-mono text-sm cursor-pointer md:text-lg dark:text-slate-300 hover:text-sky-700"><NavLink to="/contact">Contact</NavLink></div>
+      <div className="font-mono text-sm cursor-pointer md:text-lg dark:text-slate-300 hover:text-sky-700">FAQ</div>
+      <button className="px-2 py-1 text-green-900 bg-white rounded-md cursor-pointer md:px-4 md:py-2 hover:bg-green-800 hover:text-white">Get Started</button>
       <button className="p-2 rounded-full cursor-pointer bg-slate-200 dark:bg-slate-700 hover:bg-sky-700"
       onClick={() => setIsDarkMode(!isDarkMode)}>
         {isDarkMode ? (<img src="src/IMAGES/light mode.png"
           alt="Toggle Theme"          className="w-6 h-6 rounded-full "
       
-      />) : (<img src="/src/IMAGES/dark mode.png"
+      />) : (<img src="src/IMAGES/dark mode.png"
           alt="Toggle Theme"          className="w-6 h-6 rounded-full"
         />)}
         
